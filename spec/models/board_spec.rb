75803ef18d52920fa15875386e2db9a49a1c94e6
Mannah <kenikall@gmail.com>
2017-03-02 14:29:12 -0800

    completes board tests

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -7,7 +7,7 @@ describe Board do
   end
 
   describe "Sets up board correctly" do
-    context "Should name spaces correctly" do
+    context "Space names" do
       cols = %w(h g f e d c b a)
       ids = []
       8.times do |i|
@@ -16,15 +16,17 @@ describe Board do
         ids << row
       end
 
-      8.times do |row|
-        8.times do |col|
-          expected_name = ids[row][col][:name]
-          it "[#{row}][#{col}] is named #{expected_name}" do
-            expect(board[row][col][:name]).to eq(expected_name)
+      it "spaces are correctly named" do
+        correct_name = true
+        8.times do |row|
+          8.times do |col|
+            expected_name = ids[row][col][:name]
+              correct_name = correct_name && board[row][col][:name] == ids[row][col][:name]
+            end
           end
+        expect(correct_name).to be_truthy
         end
       end
-    end
 
     context "Pieces start in the correct locations" do
       it "black pawns are correctly placed" do
@@ -51,8 +53,8 @@ describe Board do
         correct_placement = correct_placement && board[0][2][:content] == 'bKnight.png'
         correct_placement = correct_placement && board[0][3][:content] == 'bQueen.png'
         correct_placement = correct_placement && board[0][4][:content] == 'bKing.png'
-        correct_placement = correct_placement && board[0][5][:content] == 'bKnight.png'
-        correct_placement = correct_placement && board[0][6][:content] == 'bBishop.png'
+        correct_placement = correct_placement && board[0][5][:content] == 'bBishop.png'
+        correct_placement = correct_placement && board[0][6][:content] == 'bKnight.png'
         correct_placement = correct_placement && board[0][7][:content] == 'bRook.png'
 
         expect(correct_placement).to be_truthy
@@ -65,11 +67,22 @@ describe Board do
         correct_placement = correct_placement && board[7][2][:content] == 'wKnight.png'
         correct_placement = correct_placement && board[7][3][:content] == 'wQueen.png'
         correct_placement = correct_placement && board[7][4][:content] == 'wKing.png'
-        correct_placement = correct_placement && board[7][5][:content] == 'wKnight.png'
-        correct_placement = correct_placement && board[7][6][:content] == 'wBishop.png'
+        correct_placement = correct_placement && board[7][5][:content] == 'wBishop.png'
+        correct_placement = correct_placement && board[7][6][:content] == 'wKnight.png'
         correct_placement = correct_placement && board[7][7][:content] == 'wRook.png'
 
         expect(correct_placement).to be_truthy
+      end
+
+      it "all spaces without pieces are empty" do
+        empty_space = true
+        (2..5).each do |row|
+          8.times do |col|
+            empty_space = empty_space && board[row][col][:content] == nil
+          end
+        end
+
+        expect(empty_space).to be_truthy
       end
     end
 
