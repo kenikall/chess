52ebeb12d02e9fbdfa2ed596d58626fd93c92ed7
Mannah <kenikall@gmail.com>
2017-03-02 14:01:52 -0800

    tests board set up

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,18 @@
 class Board < ApplicationRecord
+
+  def board
+    build_board if !@board
+    return @board
+  end
+
+  private
+
   def build_board
+    name_spaces
+    set_pieces
+  end
+
+  def name_spaces
     cols = %w(h g f e d c b a)
     @board = []
     8.times do |i|
@@ -7,11 +20,28 @@ class Board < ApplicationRecord
       8.times{ |k| row << {name: (cols[k])+(i+1).to_s, content: nil}}
       @board << row
     end
-    set_peices
+    @board
   end
 
   def set_pieces
     @board[1].each do |space|
+      space[:content] = 'bPawn.png'
     end
+
+    @board[6].each do |space|
+      space[:content] = 'wPawn.png'
+    end
+
+    @board[0][0][:content], @board[0][7][:content] = 'bRook.png', 'bRook.png'
+    @board[0][1][:content], @board[0][5][:content] = 'bBishop.png', 'bBishop.png'
+    @board[0][2][:content], @board[0][6][:content] = 'bKnight.png', 'bKnight.png'
+    @board[0][3][:content] = 'bQueen.png'
+    @board[0][4][:content] = 'bKing.png'
+
+    @board[7][0][:content], @board[7][7][:content] = 'wRook.png', 'wRook.png'
+    @board[7][1][:content], @board[7][5][:content] = 'wBishop.png', 'wBishop.png'
+    @board[7][2][:content], @board[7][6][:content] = 'wKnight.png', 'wKnight.png'
+    @board[7][3][:content] = 'wQueen.png'
+    @board[7][4][:content] = 'wKing.png'
   end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe Board do
+
   let(:board) do
-    Board.create.build_board
+    Board.create.board
   end
 
   describe "Sets up board correctly" do
     context "Should name spaces correctly" do
-      # board = test.build_board
       cols = %w(h g f e d c b a)
       ids = []
       8.times do |i|
@@ -26,9 +26,53 @@ describe Board do
       end
     end
 
-    context "Pieces start in the correct locations "
-  end
-  # it "Should initiate board with pieces in correct locations" do
+    context "Pieces start in the correct locations" do
+      it "black pawns are correctly placed" do
+        correct_placement = true
+        8.times do |col|
+          correct_placement = false if board[1][col][:content] != 'bPawn.png'
+        end
+        expect(correct_placement).to be_truthy
+      end
 
-  # end
+      it "white pawns are correctly placed" do
+        correct_placement = true
+        8.times do |col|
+          correct_placement = false if board[6][col][:content] != 'wPawn.png'
+        end
+        expect(correct_placement).to be_truthy
+      end
+
+      it "black pieces are correctly placed" do
+        correct_placement = true
+
+        correct_placement = correct_placement && board[0][0][:content] == 'bRook.png'
+        correct_placement = correct_placement && board[0][1][:content] == 'bBishop.png'
+        correct_placement = correct_placement && board[0][2][:content] == 'bKnight.png'
+        correct_placement = correct_placement && board[0][3][:content] == 'bQueen.png'
+        correct_placement = correct_placement && board[0][4][:content] == 'bKing.png'
+        correct_placement = correct_placement && board[0][5][:content] == 'bKnight.png'
+        correct_placement = correct_placement && board[0][6][:content] == 'bBishop.png'
+        correct_placement = correct_placement && board[0][7][:content] == 'bRook.png'
+
+        expect(correct_placement).to be_truthy
+      end
+
+      it "white pieces are correctly placed" do
+        correct_placement = true
+        correct_placement = correct_placement && board[7][0][:content] == 'wRook.png'
+        correct_placement = correct_placement && board[7][1][:content] == 'wBishop.png'
+        correct_placement = correct_placement && board[7][2][:content] == 'wKnight.png'
+        correct_placement = correct_placement && board[7][3][:content] == 'wQueen.png'
+        correct_placement = correct_placement && board[7][4][:content] == 'wKing.png'
+        correct_placement = correct_placement && board[7][5][:content] == 'wKnight.png'
+        correct_placement = correct_placement && board[7][6][:content] == 'wBishop.png'
+        correct_placement = correct_placement && board[7][7][:content] == 'wRook.png'
+
+        expect(correct_placement).to be_truthy
+      end
+    end
+
+  end
+
 end
